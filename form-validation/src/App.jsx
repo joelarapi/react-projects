b961086128275aad6939a8e54ce58e49e23d0251
joelarapi <arapijoel05@gmail.com>
2024-03-20 16:50:08 +0100

    Fixed the form login issues

--- form-validation/src/App.jsx
+++ form-validation/src/App.jsx
@@ -1,12 +1,38 @@
+import Register from './components/Register';
+import Login from './components/Login';
+import Home from './components/Home';
+import Layout from './components/Layout';
+import Editor from './components/Editor';
+import Admin from './components/Admin';
+import Missing from './components/Missing';
+import Unauthorized from './components/Unauthorized';
+import Lounge from './components/Lounge';
+import LinkPage from './components/LinkPage';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
 
-
   return (
-    <>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
 
-    </>
-  )
+
+          <Route path="/" element={<Home />} />
+          <Route path="editor" element={<Editor />} />
+          <Route path="admin" element={<Admin />} />
+          <Route path="lounge" element={<Lounge />} />
+
+
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
