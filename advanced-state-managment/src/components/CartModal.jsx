4ac4301a79f625eeb3dd3b9cb7f2f82795e9604a
joelarapi <arapijoel05@gmail.com>
2024-04-01 13:02:58 +0200

    finished 10th section

--- advanced-state-managment/src/components/CartModal.jsx
+++ advanced-state-managment/src/components/CartModal.jsx
@@ -1,47 +1,32 @@
-import { useContext } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
-import { CartContext } from '../store/shopping-cart-context.jsx';
+import Cart from './Cart.jsx';
 
-export default function Cart() {
-  const { items, updateItemQuantity } = useContext(CartContext);
+const CartModal = forwardRef(function Modal(
+  { title, actions },
+  ref
+) {
+  const dialog = useRef();
 
-  const totalPrice = items.reduce(
-    (acc, item) => acc + item.price * item.quantity,
-    0
+  useImperativeHandle(ref, () => {
+    return {
+      open: () => {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog id="modal" ref={dialog}>
+      <h2>{title}</h2>
+      <Cart />
+      <form method="dialog" id="modal-actions">
+        {actions}
+      </form>
+    </dialog>,
+    document.getElementById('modal')
   );
-  const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+});
 
-  return (
-    <div id="cart">
-      {items.length === 0 && <p>No items in cart!</p>}
-      {items.length > 0 && (
-        <ul id="cart-items">
-          {items.map((item) => {
-            const formattedPrice = `$${item.price.toFixed(2)}`;
-
-            return (
-              <li key={item.id}>
-                <div>
-                  <span>{item.name}</span>
-                  <span> ({formattedPrice})</span>
-                </div>
-                <div className="cart-item-actions">
-                  <button onClick={() => updateItemQuantity(item.id, -1)}>
-                    -
-                  </button>
-                  <span>{item.quantity}</span>
-                  <button onClick={() => updateItemQuantity(item.id, 1)}>
-                    +
-                  </button>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      )}
-      <p id="cart-total-price">
-        Cart Total: <strong>{formattedTotalPrice}</strong>
-      </p>
-    </div>
-  );
-}
+export default CartModal;
