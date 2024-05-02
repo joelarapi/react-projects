17013de3ab1d7d47b1e667caf0dc183e7466339c
joelarapi <arapijoel05@gmail.com>
2024-05-02 11:07:34 +0200

    beginner-routing-project-done

---
+++ routing-project/src/App.js
@@ -0,0 +1,37 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // createRoutesFromElements,
+  // Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import RootLayout from "./pages/Root";
+import Error from "./pages/Error";
+import ProductDetail from "./pages/ProductDetail";
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
+
+const router = createBrowserRouter([
+  {path:'/',
+  element: <RootLayout/>,
+   errorElement: <Error/>,
+  children:[
+    { index: true, element: <Home /> },
+    { path: "products", element: <Products /> },
+    { path: "products/:productId", element: <ProductDetail/>}
+  ]
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
