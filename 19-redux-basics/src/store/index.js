6dd8d1430562d363703a3ea8eb81f924db40e841
joelarapi <arapijoel05@gmail.com>
2024-07-24 16:09:41 +0200

    ...

---
+++ 19-redux-basics/src/store/index.js
@@ -0,0 +1,31 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const initialSate = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialSate,
+  reducers:{
+    increment(state){
+      state.counter++;
+    },
+    decrement(state){
+      state.counter--;
+    },
+    increase(state, action){
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state){
+      state.showCounter  = !state.showCounter;
+    }
+  }
+})
+
+
+
+const store = configureStore({
+  reducer : counterSlice.reducer
+});
+
+export default store;
+export const counterActions = counterSlice.actions.toggleCounter
